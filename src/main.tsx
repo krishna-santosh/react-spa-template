e667af31c357b00207224114204725822fd3fb2c
Krishna Santosh <75202541+krishna-santosh@users.noreply.github.com>
2025-01-20 14:11:50 +0530

    fix: replace react-router-dom imports with react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router";
 
 import "./global.css";
 import router from "./router";
