57265eb248fc37a3f4789e5703efbcf9d3ea8e90
Krishna Santosh <75202541+krishna-santosh@users.noreply.github.com>
2025-01-20 14:07:09 +0530

    rm tailwind.config.js

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx,mdx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
-};
