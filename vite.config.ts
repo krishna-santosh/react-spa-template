47c59307881f9b312118c365c8f407017e111288
Krishna Santosh <75202541+krishna-santosh@users.noreply.github.com>
2025-01-20 14:10:10 +0530

    feat: add tailwindcss plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 import PWA_CONFIG from "./vite-pwa-config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA(PWA_CONFIG)],
+  plugins: [react(), tailwindcss(), VitePWA(PWA_CONFIG)],
 });
